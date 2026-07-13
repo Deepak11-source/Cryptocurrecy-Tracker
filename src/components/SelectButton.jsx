8787b4f84b08e7a59ea40100b58f450793db81f5
Deepak Naidu <deepaknaidu1122@gmail.com>
2026-07-13 10:38:23 +0530

    Hoist SelectButton's styled() definition to module scope

--- src/components/SelectButton.jsx
+++ src/components/SelectButton.jsx
@@ -1,26 +1,30 @@
 import { styled } from '@mui/system';
 
-const SelectButton = ({ children, selected, onClick }) => {
-  const SelectButton = styled('span')(({ selected }) => ({
-    border: '1px solid gold',
-    borderRadius: 5,
-    padding: 10,
-    paddingLeft: 20,
-    paddingRight: 20,
-    fontFamily: 'Montserrat',
-    cursor: 'pointer',
-    backgroundColor: selected ? 'gold' : '',
-    color: selected ? 'black' : '',
-    fontWeight: selected ? 700 : 500,
-    margin: '10px',
-    '&:hover': {
-      backgroundColor: 'gold',
-      color: 'black',
-    },
-    width: '22%',
-  }));
+const StyledSelectButton = styled('span')(({ selected }) => ({
+  border: '1px solid gold',
+  borderRadius: 5,
+  padding: 10,
+  paddingLeft: 20,
+  paddingRight: 20,
+  fontFamily: 'Montserrat',
+  cursor: 'pointer',
+  backgroundColor: selected ? 'gold' : '',
+  color: selected ? 'black' : '',
+  fontWeight: selected ? 700 : 500,
+  margin: '10px',
+  '&:hover': {
+    backgroundColor: 'gold',
+    color: 'black',
+  },
+  width: '22%',
+}));
 
-  return <SelectButton onClick={onClick}>{children}</SelectButton>;
+const SelectButton = ({ children, selected, onClick }) => {
+  return (
+    <StyledSelectButton selected={selected} onClick={onClick}>
+      {children}
+    </StyledSelectButton>
+  );
 };
 
 export default SelectButton;
